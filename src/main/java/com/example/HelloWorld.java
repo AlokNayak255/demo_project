09a568eb737afbc22f40646c843eea1e920ebd2c
Alok <aloknayak0207@gmail.com>
2025-05-04 16:59:53 +0530

    removing unused variables

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -4,17 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		int i=10;
-
-		int j=20;
-		String name="Ashokit";
-		int k=30;
-		boolean status=false;
-		double salary=1000000.98;
 		
-
-		String s="Alok";
-		double salary=10000.89;
 
 	}
 
